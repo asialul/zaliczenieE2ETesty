6f281ece63a2393a0a1d8cbce7047f5224d99462
Asia Lula <asialul@gmail.com>
2024-06-09 18:44:34 +0200

    pierwszy test działa

---
+++ cypress/e2e/main_page.cy.js
@@ -0,0 +1,18 @@
+describe('main page navigation tests', () => {
+
+  beforeEach(() => {
+    // base URL - 'http://localhost:5173'
+    cy.visit('/')
+  })
+  
+  it('check headings are correct', () => {
+    cy.get('main').find('h1').contains(/wybierz szyfr/i)
+    cy.get('ul').should('have.class', 'lista-szyfry').then(el => {
+      expect(el).has.length(2)
+    })
+
+  it('check links are correctly defined', () => {
+    
+  })
+  })
+})
